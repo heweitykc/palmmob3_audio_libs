32a18717802813f0b1702af5d1289dbfa7402309
贺威 <240100889@qq.com>
2021-08-23 09:33:47 +0800

    audio

--- react-native-audio-library/Example/App.js
+++ react-native-audio-library/Example/App.js
@@ -2,44 +2,54 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View,NativeModules,TouchableHighlight,Image,DeviceEventEmitter } from 'react-native';
 
-// const { AudioLibrary } = NativeModules;
 
-import AudioLibrary from 'react-native-audio-library';
+import * as AudioLibrary from 'react-native-audio-library';
 import * as FileSystem from 'expo-file-system';
 import * as DocumentPicker from 'expo-document-picker';
 
 let mp4Resource = require('./res/download.mp4');
 
 const testFunc = async () => {
-  AudioLibrary.sampleMethod("123", 123, function(str){
-    console.log('callback1:' + str);
-  });
   let mp4path = Image.resolveAssetSource(mp4Resource).uri;
 
   let mp3path = FileSystem.documentDirectory + "test.mp3";
 
-  AudioLibrary.GetMediaInfo(mp3path, 0, function(str){
+  console.log(AudioLibrary);
+
+  AudioLibrary.getMediaInfo(mp3path, 0, function(str){
     console.log('callback2:' + str);
   });
 
   let result = await FileSystem.getInfoAsync(mp3path, {});
   console.log(result);
 
-  return;
+  let result1 = await AudioLibrary.getSupportFormat(1);
+  let result2 = await AudioLibrary.getSupportFormat(2);
+  console.log(result1);
+  console.log(result2);
 
-  // let result = await DocumentPicker.getDocumentAsync({type: "*/*" });  
-  // console.log(result);
-  // mp4path = result['uri'];
-  // let cmd = ["ffmpeg", "-i", mp4path, "-f", "mp3", "-vn", mp3path];
-  // AudioLibrary.RunCmd(cmd, function(ret){
-  //   console.log('callback3:' + ret);
-  // })
 
-  // this.boxStatus = DeviceEventEmitter.addListener("ffmpeg_evt", ((msg) => {
+  let result3 = await DocumentPicker.getDocumentAsync({type: "*/*" });  
+  console.log(result3);
+  mp4path = result['uri'];
+  let cmd = ["ffmpeg", "-i", mp4path, "-f", "mp3", "-vn", mp3path];
+  AudioLibrary.runCmd(cmd, function(ret){
+    console.log('callback3:' + ret);
+  })
+
+  AudioLibrary.listenAudioLibraryEvent("AudioLibrary_start", ((msg) => {
+      console.log(msg);
+  }));
+
+  // listenAudioLibraryEvent("AudioLibrary_err", ((msg) => {
   //     console.log(msg);
   // }));
 
-  // this.boxStatus2 = DeviceEventEmitter.addListener("ffmpeg_complete", (async (msg) => {
+  // listenAudioLibraryEvent("AudioLibrary_progress", ((msg) => {
+  //     console.log(msg);
+  // }));
+
+  // listenAudioLibraryEvent("AudioLibrary_complete", (async (msg) => {
   //   let result = await FileSystem.getInfoAsync(mp3path, null);
   //    console.log(result);
   // }));  
--- react-native-audio-library/index.js
+++ react-native-audio-library/index.js
@@ -4,4 +4,33 @@ import { NativeModules } from 'react-native';
 
 const { AudioLibrary } = NativeModules;
 
-export default AudioLibrary;
+export function listenAudioLibraryEvent(eventName, callback){
+    const eventEmitter = new NativeEventEmitter(AudioLibrary);
+    this.eventListener = eventEmitter.addListener(eventName, (event) => {
+        callback(event);
+     });
+}
+
+export function getMediaInfo(fpath, attr){
+    return AudioLibrary.getMediaInfo(fpath, attr);
+}
+
+export function runCmd(cmd){
+    return AudioLibrary.runCmd(cmd);
+}
+
+export function setDebug(debug){
+    return AudioLibrary.setDebug(debug);
+}
+
+export function getSupportFormat(formatType){
+    return AudioLibrary.getSupportFormat(formatType);
+}
+
+export function getSupportCodec(codecType){
+    return AudioLibrary.getSupportCodec(codecType);
+}
+
+export function cancelTask(){
+    AudioLibrary.cancel();
+}
